77b3de3689efcc3b3bd7c28c9378b4d184dfc59c
Rahul Kankariya <63840385+rahulkankariya@users.noreply.github.com>
2025-09-26 17:27:21 +0530

    api base url page redirect in html

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+      <h1>To Do List API</h1>
+      <p>Use <a href="http://localhost:3000/api" target="_blank">/todo</a> endpoint to manage your tasks.</p>
+    `;
   }
 }
